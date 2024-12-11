57af901f2244eac62637c8876665a473f50f0d2e
Jitse De Smet <jitse@reedonk8.be>
2024-12-11 10:56:40 +0100

    refactor tests

--- test/query.bench.ts
+++ test/query.bench.ts
@@ -1,19 +1,11 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 import { Parser } from 'sparqljs';
-import { Bench } from 'tinybench';
+import { describe, bench } from 'vitest';
 import { SparqlParser } from '../src/parser/sparql11/SparqlParser.js';
 
-async function main(): Promise<void> {
-  const bench = new Bench({
-    iterations: 100,
-    throws: true,
-  });
-
-  bench.concurrency = null;
-
+describe('query, exclude construction', () => {
   const newParser = new SparqlParser();
   const oldParser = new Parser();
-
   const query = `
 SELECT ?president ?party ?page WHERE {
    ?president <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/President> .
@@ -24,18 +16,10 @@ SELECT ?president ?party ?page WHERE {
 }
 `;
 
-  bench
-    .add('TRAQULA parse', () => {
-      const res = newParser.parse(query);
-    })
-    .add('sparqljs', () => {
-      const res = oldParser.parse(query);
-    });
-
-  // Await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
-  await bench.run();
-
-  console.table(bench.table());
-}
-
-main().catch(console.error);
+  bench('TRAQULA parse', () => {
+    const res = newParser.parse(query);
+  });
+  bench('sparqljs', () => {
+    const res = oldParser.parse(query);
+  });
+});
--- test/query.test.ts
+++ test/query.test.ts
@@ -2,6 +2,7 @@
 import * as fs from 'node:fs';
 import * as fsp from 'node:fs/promises';
 import { DataFactory } from 'rdf-data-factory';
+import type { TestFunction } from 'vitest';
 import { beforeEach, describe, it } from 'vitest';
 import './matchers/toEqualParsedQuery.js';
 import { SparqlParser } from '../src/parser/sparql11/SparqlParser.js';
@@ -48,8 +49,8 @@ describe('a SPARQL parser', () => {
     }
   });
 
-  function testErrorousQuery(testName: string, query: string, errorMsg: string): void {
-    it(`should throw an error on ${testName}`, ({ expect }) => {
+  function testErroneousQuery(query: string, errorMsg: string): TestFunction<object> {
+    return ({ expect }) => {
       let error: any = null;
       try {
         parser.parse(query);
@@ -59,22 +60,20 @@ describe('a SPARQL parser', () => {
       expect(error).not.toBeUndefined();
       expect(error).toBeInstanceOf(Error);
       expect(error.message).toContain(errorMsg);
-    });
+    };
   }
 
-  testErrorousQuery('an invalid query', 'invalid', 'Parse error on line 1');
+  it('should throw an error on an invalid query', testErroneousQuery('invalid', 'Parse error on line 1'));
 
-  testErrorousQuery(
-    'a projection of ungrouped variable',
+  it('should throw an error on a projection of ungrouped variable', testErroneousQuery(
     'PREFIX : <http://www.example.org/> SELECT ?o WHERE { ?s ?p ?o } GROUP BY ?s',
     'Projection of ungrouped variable (?o)',
-  );
+  ));
 
-  testErrorousQuery(
-    'an invalid selectscope',
+  it('should throw an error on an invalid selectscope', testErroneousQuery(
     'SELECT (1 AS ?X ) { SELECT (2 AS ?X ) {} }',
     'Target id of \'AS\' (?X) already used in subquery',
-  );
+  ));
 
   it('should preserve BGP and filter pattern order', ({ expect }) => {
     const query = 'SELECT * { ?s ?p "1" . FILTER(true) . ?s ?p "2"  }';
@@ -189,11 +188,10 @@ describe('a SPARQL parser', () => {
     });
   });
 
-  testErrorousQuery(
-    ' relative IRIs if no base IRI is specified',
+  it('should throw an error on relative IRIs if no base IRI is specified', testErroneousQuery(
     'SELECT * { <a> <b> <c> }',
     'Cannot resolve relative IRI a because no base IRI was set.',
-  );
+  ));
 
   describe('with group collapsing disabled', () => {
     it('should keep explicit pattern group', ({ expect }) => {
@@ -276,11 +274,10 @@ describe('a SPARQL parser', () => {
   });
 
   describe('for update queries', () => {
-    testErrorousQuery(
-      'blank nodes in DELETE clause',
+    it('should throw an error on blank nodes in DELETE clause', testErroneousQuery(
       'DELETE { ?a <ex:knows> [] . } WHERE { ?a <ex:knows> "Alan" . }',
       'Detected illegal blank node in BGP',
-    );
+    ));
 
     it('should not throw on blank nodes in INSERT clause', ({ expect }) => {
       const query = 'INSERT { ?a <ex:knows> [] . } WHERE { ?a <ex:knows> "Alan" . }';
@@ -288,41 +285,30 @@ describe('a SPARQL parser', () => {
       expect(parser.parse(query)).toMatchObject({});
     });
 
-    testErrorousQuery(
-      'blank nodes in compact DELETE clause',
+    it('should throw an error on blank nodes in compact DELETE clause', testErroneousQuery(
       'DELETE WHERE { _:a <ex:p> <ex:o> }',
       'Detected illegal blank node in BGP',
-    );
+    ));
 
-    testErrorousQuery(
-      'variables in DELETE DATA clause',
+    it('should throw an error on variables in DELETE DATA clause', testErroneousQuery(
       'DELETE DATA { ?a <ex:p> <ex:o> }',
       'Detected illegal variable in BGP',
-    );
+    ));
 
-    testErrorousQuery(
-      'blank nodes in DELETE DATA clause',
+    it('should throw an error on blank nodes in DELETE DATA clause', testErroneousQuery(
       'DELETE DATA { _:a <ex:p> <ex:o> }',
       'Detected illegal blank node in BGP',
-    );
+    ));
 
-    testErrorousQuery(
-      'variables in DELETE DATA clause with GRAPH',
+    it('should throw an error on variables in DELETE DATA clause with GRAPH', testErroneousQuery(
       'DELETE DATA { GRAPH ?a { <ex:s> <ex:p> <ex:o> } }',
       'Detected illegal variable in GRAPH',
-    );
+    ));
 
-    testErrorousQuery(
-      'variables in INSERT DATA clause',
+    it('should throw an error on variables in INSERT DATA clause', testErroneousQuery(
       'INSERT DATA { ?a <ex:p> <ex:o> }',
       'Detected illegal variable in BGP',
-    );
-
-    testErrorousQuery(
-      'variables in DELETE DATA clause with GRAPH',
-      'DELETE DATA { GRAPH ?a { <ex:s> <ex:p> <ex:o> } }',
-      'Detected illegal variable in GRAPH',
-    );
+    ));
 
     it('should not throw on reused blank nodes in one INSERT DATA clause', ({ expect }) => {
       const query = 'INSERT DATA { _:a <ex:p> <ex:o> . _:a <ex:p> <ex:o> . }';
@@ -330,17 +316,15 @@ describe('a SPARQL parser', () => {
       expect(parser.parse(query)).toMatchObject({});
     });
 
-    testErrorousQuery(
-      'reused blank nodes across INSERT DATA clauses',
+    it('should throw an error on reused blank nodes across INSERT DATA clauses', testErroneousQuery(
       'INSERT DATA { _:a <ex:p> <ex:o> }; INSERT DATA { _:a <ex:p> <ex:o> }',
       'Detected reuse blank node across different INSERT DATA clauses',
-    );
+    ));
 
-    testErrorousQuery(
-      'reused blank nodes across INSERT DATA clauses with GRAPH',
+    it('should throw an error on reused blank nodes across INSERT DATA clauses with GRAPH', testErroneousQuery(
       'INSERT DATA { _:a <ex:p> <ex:o> }; INSERT DATA { GRAPH <ex:g> { _:a <ex:p> <ex:o> } }',
       'Detected reuse blank node across different INSERT DATA clauses',
-    );
+    ));
 
     it('should not throw on comment between INSERT and DATA', ({ expect }) => {
       const query = `INSERT
@@ -357,18 +341,16 @@ DATA { GRAPH <ex:G> { <ex:s> <ex:p> 'o1', 'o2', 'o3' } }`;
       expect(parser.parse(query)).toMatchObject({});
     });
 
-    testErrorousQuery(
-      'commented DATA after INSERT',
+    it('should throw an error on commented DATA after INSERT', testErroneousQuery(
       'INSERT # DATA { GRAPH <ex:G> { <ex:s> <ex:p> \'o1\', \'o2\', \'o3\' } }',
       'Parse error',
-    );
+    ));
   });
 
-  testErrorousQuery(
-    'unicode codepoint escaping in literal with partial surrogate pair',
+  it('should throw an error on unicode codepoint escaping in literal with partial surrogate pair', testErroneousQuery(
     'SELECT * WHERE { ?s <ex:p> \'\uD800\' }',
     'Invalid unicode codepoint of surrogate pair without corresponding codepoint',
-  );
+  ));
 
   it(
     'should not throw an error on unicode codepoint escaping in literal with complete surrogate pair',
